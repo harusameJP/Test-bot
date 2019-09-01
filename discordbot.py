34ac069b719a4c98b18434d8c4fb15bc7ac92171
torimotsu <53377948+torimotsu@users.noreply.github.com>
2019-09-01 17:57:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,17 @@
-import Discord
+import discord
 
-TOKEN = 'NjE3NjMyNDYyOTYwNDU5Nzc5.XWt9SA.IdhKFxURxyygPDwKxwzRTE72x5g'
-
-client = discord.Client()
+client = discord.Client(NjE3NjMyNDYyOTYwNDU5Nzc5.XWt9SA.IdhKFxURxyygPDwKxwzRTE72x5g)
 
 @client.event
 async def on_ready():
-    print('LoginSuccess!')
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_ready():
-    
-    if message.author.bot:
-    return
-if message.content == '/help':
-    await message.channel.send('このbotはテスト用です。')
-    
-    client.run('NjE3NjMyNDYyOTYwNDU5Nzc5.XWt9SA.IdhKFxURxyygPDwKxwzRTE72x5g')
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NjE3NjMyNDYyOTYwNDU5Nzc5.XWt9SA.IdhKFxURxyygPDwKxwzRTE72x5g')
